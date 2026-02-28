c096ad8c91fb6a5d9f157b5b8747208415ba5b71
Maimunat <lawalm281@gmail.com>
2026-02-28 23:48:36 +0100

    God please

--- breastcancer/settings.py
+++ breastcancer/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("django-insecure-*)va_1tzt)zn7n_9kej_tbsj*v(gg$85nlz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = [os.environ.get('breastcancer.onrender.com', 'localhost'), '127.0.0.1']
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost'), '127.0.0.1', '.onrender.com']
 
 
 
